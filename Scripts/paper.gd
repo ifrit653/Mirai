f07c269f18003da2b8f872e79ce067bf4b72b15f
Figo <ifrit653@gmail.com>
2025-10-19 16:21:02 +0300

    add hover features to the paper

--- Scripts/paper.gd
+++ Scripts/paper.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+var shader = preload("res://Shader/suspect.gdshader")
+@onready var  paper_sprite : Sprite2D = $Sprite2D 
+
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		show_paper()
@@ -20,3 +23,12 @@ func show_paper():
 	var viewport_rect = get_viewport().get_visible_rect()
 	var paper_size = paper_view.paper_sprite.texture.get_size() * paper_view.paper_sprite.scale
 	paper_view.position = viewport_rect.size / 2 - paper_size / 2
+
+
+func _on_mouse_entered() -> void:
+	var material = ShaderMaterial.new()
+	material.shader = shader
+	paper_sprite.material = material
+
+func _on_mouse_exited() -> void:
+	paper_sprite.material = null
